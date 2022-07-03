7eaf023a4dfa0f2b9291b1bd1bd7f0c73cf59049
Grigory <grishavinyar@icloud.com>
2022-07-03 22:44:33 +0300

    Got coordinates from Geocoder

--- ToDoApp/ToDoApp/Controller/NewTaskVC.swift
+++ ToDoApp/ToDoApp/Controller/NewTaskVC.swift
@@ -6,23 +6,47 @@
 //
 
 import UIKit
+import CoreLocation
 
 class NewTaskVC: UIViewController {
 
+    var taskManager: TaskManager!
+    var geocoder = CLGeocoder()
+    
     @IBOutlet var titleTF: UITextField!
     @IBOutlet var locationTF: UITextField!
     @IBOutlet var dateTF: UITextField!
-    @IBOutlet var adressTF: UITextField!
+    @IBOutlet var addressTF: UITextField!
     @IBOutlet var saveButton: UIButton!
     @IBOutlet var cancelButton: UIButton!
+    @IBOutlet var descriptionTF: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBAction func save() {
+        
+        guard let titleString = titleTF.text else { return }
+        guard let locationString = locationTF.text else { return }
+        guard let dateText = dateTF.text else { return }
+        
+        let date = dateFormatter.date(from: dateText)
+        let descriptionString = descriptionTF.text
+        let addressString = addressTF.text
+        
+        guard let address = addressString else { return }
+        geocoder.geocodeAddressString(address) { [weak self](placemarks, error) in
+            let placemark = placemarks?.first
+            let coordinate = placemark?.location?.coordinate
+            let location = Location(name: locationString, coordinate: coordinate)
+            let task = Task(title: titleString, description: descriptionString, location: location, date: date)
+            
+            self?.taskManager.add(task: task)
+        }
     }
     
-
+    var dateFormatter: DateFormatter {
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        return df
+    }
     
 
 }
